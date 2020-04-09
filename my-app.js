1419d2ca25a35ba0eaeec61f3094b0c482c95e54
brq-cr <b.rq@me.com>
2020-04-08 22:52:40 -0600

    poc

--- my-app.js
+++ my-app.js
@@ -1,72 +1,48 @@
 import { LitElement, html, css } from 'lit-element';
+import { BehaviorSubject, Subject, combineLatest } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { openWc } from './open-wc';
 
 
-class MyApp extends LitElement {  
-  static get styles() {
-    return [
-      css`
-        :host {
-          text-align: center;
-          min-height: 100vh;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          font-size: calc(10px + 2vmin);
-          color: #1a2b42;
-        }
+class MyApp extends LitElement {
+  elementDisconnect$ = new Subject();
+  text$ = new BehaviorSubject('holis');
 
-        header {
-          margin: auto;
-        }
+  get text() {
+    return this.text$.getValue();
+  }
 
-        svg {
-          animation: app-logo-spin infinite 20s linear;
-        }
+  connectedCallback() {
+    combineLatest(
+      this.text$
+    ).pipe(
+      takeUntil(this.elementDisconnect$),
+    ).subscribe(() => this.requestUpdate());
+    super.connectedCallback();
+  }
 
-        a {
-          color: #217FF9;
-        }
-
-        .app-footer {
-          color: #a8a8a8;
-          font-size: calc(10px + 0.5vmin);
-        }
-
-        @keyframes app-logo-spin {
-          from {
-            transform: rotate(0deg);
-          }
-          to {
-            transform: rotate(360deg);
-          }
-        }
-      `
-    ]
+  disconnectedCallback() {
+    this.elementDisconnect$.next();
+    this.elementDisconnect$.unsubscribe();
+    super.disconnectedCallback();
   }
 
   render() {
     return html`
-      <header class="app-header">
-        <a href="https://open-wc.org" target="_blank">
-          ${openWc}
-        </a>
-        <h1>open-wc</h1>
-        <p>
-          Edit <code>src/my-app.js</code> and save to reload.
-        </p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/developing/#examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </header>
-      <p class="app-footer">🚽 Made with love by <a target="_blank" href="https://github.com/open-wc">open-wc</a>.</p>
-    `;
+    <div>
+      ${this.text}
+      <button 
+        type="button" 
+        @click="${e => this.updateMessage(e) }">
+          Cambiar texto
+      </button>
+    </div>`;
+  }
+
+  updateMessage(e) {
+    e.preventDefault();
+    console.log(e);
+    this.text$.next('algo mas');
   }
 }
 
